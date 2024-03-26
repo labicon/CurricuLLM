3da4dd7b480f2f711f8d64c7e6dea7cf0e7276b0
kh-ryu <kr37@illinois.edu>
2024-03-26 13:20:02 -0500

    initial ant version

--- environments/Curriculum/__init__.py
+++ environments/Curriculum/__init__.py
@@ -20,31 +20,31 @@ register(
 
 register(
      id="Curriculum/AntMaze_UMaze-v0",
-     entry_point="Curriculum.envs.ant_maze_v0:AntMazeEnv",
+     entry_point="Curriculum.envs.AntMaze_UMaze_v0:AntMazeEnv",
      max_episode_steps=700,
 )
 
 register(
      id="Curriculum/AntMaze_UMaze-v1",
-     entry_point="Curriculum.envs.ant_maze_v1:AntMazeEnv",
+     entry_point="Curriculum.envs.AntMaze_UMaze_v1:AntMazeEnv",
      max_episode_steps=700,
 )
 
 register(
      id="Curriculum/AntMaze_UMaze-v2",
-     entry_point="Curriculum.envs.ant_maze_v2:AntMazeEnv",
+     entry_point="Curriculum.envs.AntMaze_UMaze_v2:AntMazeEnv",
      max_episode_steps=700,
 )
 
 register(
      id="Curriculum/AntMaze_UMaze-v3",
-     entry_point="Curriculum.envs.ant_maze_v3:AntMazeEnv",
+     entry_point="Curriculum.envs.AntMaze_UMaze_v3:AntMazeEnv",
      max_episode_steps=700,
 )
 
 register(
      id="Curriculum/AntMaze_UMaze-v4",
-     entry_point="Curriculum.envs.ant_maze_v4:AntMazeEnv",
+     entry_point="Curriculum.envs.AntMaze_UMaze_v4:AntMazeEnv",
      max_episode_steps=700,
 )
 

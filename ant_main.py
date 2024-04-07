cb1c5422027792100df05523c5d53011c6550b43
kh-ryu <kr37@illinois.edu>
2024-04-07 01:17:10 -0500

    Add reward addition experiments module

--- ant_main.py
+++ ant_main.py
@@ -1,27 +1,28 @@
 import gymnasium as gym
-import os
-import numpy as np
-import pandas as pd
-
-from stable_baselines3 import PPO, SAC
-from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
-from stable_baselines3.common.logger import configure
-
-from evaluation.evalcallback_feedback import CurriculumEvalCallback
+import gc
+import torch
 
 from utils.train_utils import *
-from train_ant import Curriculum_Module
+from train_ant import Curriculum_Module, Reward_Addition_Module
 
 if __name__ == "__main__":
     env_name = "AntMaze_UMaze"
     env_path = "./environments/Curriculum/envs/AntMaze_UMaze.py"
     logger_path = "./logs/AntMaze_UMaze_3/"
 
-    # Generate curriculum module
+    # Curriculum experiments
     curriculum_module = Curriculum_Module(env_name, env_path, logger_path)
-
-    # Generate curriculum and return list of dictionaries with task details
     curriculum_module.generate_curriculum()
-    
-    # Train curriculum
     curriculum_module.train_curriculum()
+
+    del curriculum_module
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    # Reward addition experiments
+    reward_addition_module = Reward_Addition_Module(env_name, env_path, logger_path)
+    reward_addition_module.train_with_reward_addition()
+
+    del reward_addition_module
+    gc.collect()
+    torch.cuda.empty_cache()
--- fetch_main.py
+++ fetch_main.py
@@ -1,27 +1,28 @@
 import gymnasium as gym
-import os
-import numpy as np
-import pandas as pd
-
-from stable_baselines3 import PPO, SAC
-from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
-from stable_baselines3.common.logger import configure
-
-from evaluation.evalcallback_feedback import CurriculumEvalCallback
+import gc
+import torch
 
 from utils.train_utils import *
-from train_fetch import Curriculum_Module
+from train_fetch import Curriculum_Module, Reward_Addition_Module
 
 if __name__ == "__main__":
     env_name = "Fetch_Push"
     env_path = "./environments/Curriculum/envs/Fetch_Push.py"
     logger_path = "./logs/Fetch/Push_6/"
 
-    # Generate curriculum module
+    # Curriculum experiments
     curriculum_module = Curriculum_Module(env_name, env_path, logger_path)
-
-    # Generate curriculum and return list of dictionaries with task details
     curriculum_module.generate_curriculum()
-    
-    # Train curriculum
     curriculum_module.train_curriculum()
+
+    del curriculum_module
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    # Reward addition experiments
+    reward_addition_module = Reward_Addition_Module(env_name, env_path, logger_path)
+    reward_addition_module.train_with_reward_addition()
+
+    del reward_addition_module
+    gc.collect()
+    torch.cuda.empty_cache()

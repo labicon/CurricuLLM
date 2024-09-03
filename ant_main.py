9db6315deaa13a6fc59e5319afed12b2f52ffb10
kh-ryu <kanghyun.ryu@berkeley.edu>
2024-09-03 13:30:10 -0700

    add sac and scratch module

--- ant_main.py
+++ ant_main.py
@@ -4,7 +4,7 @@ import torch
 import argparse
 
 from utils.train_utils import *
-from train_ant import Curriculum_Module, HER_Module
+from train_ant import Curriculum_Module, HER_Module, SAC_Module, Scratch_Module
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Recieve seed number")
@@ -18,9 +18,9 @@ if __name__ == "__main__":
 
     # Curriculum experiments
     curriculum_module = Curriculum_Module(env_name, env_path, logger_path, seed)
-    # curriculum_module.generate_curriculum()
-    # curriculum_module.train_curriculum()
-    curriculum_module.resume_curriculum(resume_idx=1, resume_sample_idx=0, resume_from_training=True)
+    curriculum_module.generate_curriculum()
+    curriculum_module.train_curriculum()
+    # curriculum_module.resume_curriculum(resume_idx=1, resume_sample_idx=0, resume_from_training=True)
 
     del curriculum_module
     gc.collect()
@@ -33,3 +33,15 @@ if __name__ == "__main__":
     # del her_module
     # gc.collect()
     # torch.cuda.empty_cache()
+
+    # # SAC experiments
+    sac_module = SAC_Module(env_name, env_path, logger_path, seed)
+    sac_module.train_with_sac()
+
+    del sac_module
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    # # Scratch experiments
+    scratch_module = Scratch_Module(env_name, env_path, logger_path, seed)
+    scratch_module.train_curriculum()
